5ae702f3d41aab207fa4214ae385b796a4dee800
Hasib Rowan <125839935+hasibrowan@users.noreply.github.com>
2023-04-06 23:39:40 -0400

    Create site.php

---
+++ site.php
@@ -0,0 +1,15 @@
+	<section id="subscribe">
+		<h2>Stay Connected</h2>
+		<p>Sign up for our newsletter to receive updates on new tutorials, events, and special offers.</p>
+		<form>
+			<input type="email" name="email" placeholder="Enter your email">
+			<button type="submit" class="button">Subscribe</button>
+		</form>
+	</section>
+</main>
+
+<footer>
+	<p>&copy; 2023 Hello World - Inspiring Code</p>
+</footer>
+
+<script src="app.js"></script>
